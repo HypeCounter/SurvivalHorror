6ad35e077132ee8b67413ceae222e7f128f894c0
WagnerCardozo <wagner.hp1999@gmail.com>
2019-05-08 00:02:42 -0300

    Zumbi na sala 2

---
+++ Assets/Scripts/SpawnarZumbi.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnarZumbi : MonoBehaviour
+{
+    [SerializeField] private Transform spawnPoint;
+    [SerializeField] private GameObject zumbi;
+
+    void OnTriggerEnter(Collider other)
+    {
+        Zumbi.Instantiate(zumbi, spawnPoint.transform.position, Quaternion.identity);
+        this.gameObject.SetActive(false);
+    }
+
+    
+}
